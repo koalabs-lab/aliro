ffa4ed5d20357357cd275f14b2349f0a9b9c270d
Carlos C <carlos@koalabs.co>
2014-07-30 18:17:21 -0500

    Added RoleTrait methods

--- src/Koalabs/Aliro/Users/Traits/RoleTrait.php
+++ src/Koalabs/Aliro/Users/Traits/RoleTrait.php
@@ -1,6 +1,7 @@
 <?php namespace Koalabs\Aliro\Users\Traits;
 
 use Config;
+use Koalabs\Aliro\Roles\Role;
 
 trait RoleTrait {
 
@@ -14,6 +15,48 @@ trait RoleTrait {
     $config = Config::get('aliro::users');
 
     return $this->belongsToMany('Koalabs\Aliro\Roles\Role', $config['pivot_table']);
+  }
+
+  /**
+   * Check if the user has a ceratin role
+   *
+   * @param  string $name
+   * @return bool
+   */
+  public function hasRole($name)
+  {
+    foreach ($this->roles as $role)
+    {
+      if ($role->name == $name) return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Assign a role to the user
+   *
+   * @param mixed  $role
+   * @return void
+   */
+  public function assignRole($role)
+  {
+    if (is_string($role)) $role = Role::whereName($role)->first();
+
+    return $this->roles()->attach($role);
+  }
+
+  /**
+   * Revoke a role from the user
+   *
+   * @param  mixed  $role
+   * @return void
+   */
+  public function revokeRole($role)
+  {
+    if (is_string($role)) $role = Role::whereName($role)->first();
+
+    return $this->roles()->detach($role);
   }
 
 }
